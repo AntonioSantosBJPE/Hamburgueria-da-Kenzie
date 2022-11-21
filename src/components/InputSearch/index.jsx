64f15ab50361a0e428753d3b53648470e840be96
Antonio <netoifpe@gmail.com>
2022-11-21 20:05:05 -0300

    fix inputSearch

--- src/components/InputSearch/index.jsx
+++ src/components/InputSearch/index.jsx
@@ -1,12 +1,26 @@
 import { useState } from "react";
 import { StyledBoxInputSearch } from "./styled";
 import { Button } from "../Button";
+
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 export function InputSearch({ setSearch, dataApi }) {
   const [inputValue, setInputValue] = useState("");
 
+  function showToastErrorMessage() {
+    toast.error("Não é possível realizar uma busca com o campo vazio!", {
+      position: toast.POSITION.TOP_CENTER,
+    });
+  }
+
   function categorySearch() {
-    setSearch(inputValue);
-    setInputValue("");
+    if (inputValue.trim() === "") {
+      showToastErrorMessage();
+    } else {
+      setSearch(inputValue.trim());
+      setInputValue("");
+    }
   }
 
   return (
